8a6040daceef09d32b845409dca2925ab058cb86
niels <niels_outlook.com>
2021-12-14 03:09:23 -0500

    prueba de concepto 2

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
+// mongoose.connect(uri, { server: { reconnectTries: Number.MAX_VALUE } });
 // mongoose.connect('mongodb://127.0.0.1:27017/database_agraria', {
 mongoose.connect('mongodb://mongo/database_agraria', {
+    // server: { reconnectTries: Number.MAX_VALUE },
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
